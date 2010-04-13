9a7e38a05f87b288060f601561bfed6f77ded41a
webdizz <webdizz@gmail.com>
2010-04-13 19:10:48 +0300

    Add abilities to generate fields by class analyzing

--- glangs-plugin/src/groovy/name/webdizz/grails/langs/ast/LocaleTransformation.groovy
+++ glangs-plugin/src/groovy/name/webdizz/grails/langs/ast/LocaleTransformation.groovy
@@ -1,41 +1,79 @@
+/* Copyright 2010 Izzet Mustafayev
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package name.webdizz.grails.langs.ast
 
 
-import java.lang.reflect.Modifier;
+import java.lang.reflect.Modifier
+import java.util.List
 
 import org.codehaus.groovy.ast.ASTNode 
-import org.codehaus.groovy.ast.AnnotationNode;
+import org.codehaus.groovy.ast.AnnotationNode
 import org.codehaus.groovy.ast.ClassNode 
+import org.codehaus.groovy.ast.FieldNode
 import org.codehaus.groovy.control.CompilePhase 
 import org.codehaus.groovy.control.SourceUnit 
 import org.codehaus.groovy.transform.ASTTransformation
 import org.codehaus.groovy.transform.GroovyASTTransformation 
 
+/**
+ * @author Izzet_Mustafayev
+ *
+ */
 @GroovyASTTransformation(phase = CompilePhase.INSTRUCTION_SELECTION)
 class LocaleTransformation implements ASTTransformation {
-	
-	static int PUBLIC = 1
-	static int STATIC = 8
 	
 	void visit(ASTNode[] astNodes, SourceUnit sourceUnit) {
 		
 		// use guard clauses as a form of defensive programming. 
 		if (!sourceUnit) return 
-		if (!astNodes) return 
-        if (!astNodes[0]) return 
-        if (!astNodes[1]) return 
-        if (!(astNodes[0] instanceof AnnotationNode)) return
-        if (astNodes[0].classNode?.name != Localizable.class.getName()) return
-        if (!(astNodes[1] instanceof ClassNode)) return 
-		def classNode = astNodes[1]
+			if (!astNodes) return 
+			if (!astNodes[0]) return 
+			if (!astNodes[1]) return 
+			if (!(astNodes[0] instanceof AnnotationNode)) return
+			if (astNodes[0].classNode?.name != Localizable.class.getName()) return
+			if (!(astNodes[1] instanceof ClassNode)) return 
+			def classNode = astNodes[1]
 		if (!classNode) return
-		injectLocalizableProperties(classNode)
+			injectLocalizableProperties(classNode)
 	}
 	
 	private void injectLocalizableProperties(ClassNode classNode) {
-		String propName = "nameRu"
-		if(!classNode.hasProperty(propName)){
-			classNode.addProperty(propName, Modifier.PUBLIC, new ClassNode(String.class), null, null, null)
+		List<FieldNode> fields = classNode.getFields()
+		List<String> localeSuffixes = ['Ru']
+		List<String> properties = []
+		//gather properties to inject
+		if(!fields.isEmpty()){
+			fields.each{ FieldNode field ->
+				if (String.class.getName() == field.getType().getName()) {
+					localeSuffixes.each{ String suffix ->
+						String propName = field.getName()+suffix
+						if(!classNode.hasProperty(propName)){
+							properties.add(propName)
+						}
+					}
+				} else {
+					println 'Bad'
+				}
+			}
+		}
+		//inject properties
+		if(!properties.isEmpty()){
+			properties.each{ String prop ->
+				classNode.addProperty(prop, Modifier.PUBLIC, new ClassNode(String.class), null, null, null)
+			}
 		}
 	}
 	
