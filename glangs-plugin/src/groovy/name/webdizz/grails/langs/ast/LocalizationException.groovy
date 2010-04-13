02c2c5d16cdd831e6fc7e9bda626b4b7a0b13ab6
webdizz <webdizz@gmail.com>
2010-04-13 17:18:25 +0300

    LocalizationException was moved to another package

--- glangs-plugin/src/groovy/name/webdizz/grails/langs/ast/LocalizationException.groovy
+++ glangs-plugin/src/groovy/name/webdizz/grails/langs/ast/LocalizationException.groovy
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package name.webdizz.grails.glint;
+package name.webdizz.grails.langs.ast;
 
 /**
  * @author Izzet Mustafayev
