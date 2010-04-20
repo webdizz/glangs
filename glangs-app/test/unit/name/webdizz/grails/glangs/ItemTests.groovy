bf625712f47a457dcef07691a150674b3df1ac52
webdizz <webdizz@gmail.com>
2010-04-20 15:06:02 +0300

    Upgrade test app to Grails 1.3 RC1

--- glangs-app/test/unit/name/webdizz/grails/glangs/ItemTests.groovy
+++ glangs-app/test/unit/name/webdizz/grails/glangs/ItemTests.groovy
@@ -1,17 +1,18 @@
 package name.webdizz.grails.glangs
 
+import org.junit.Test
+
 import grails.test.*
+import static grails.test.MockUtils.mockDomain
+import static org.junit.Assert.assertNotNull
 
-class ItemTests extends GrailsUnitTestCase {
-    protected void setUp() {
-        super.setUp()
-    }
-
-    protected void tearDown() {
-        super.tearDown()
-    }
-
+class ItemTests {
+    
+	@Test
     void testSomething() {
-
+		mockDomain(Item, [:])
+    	def item = new Item()
+		item.nameRu = ''
+		assertNotNull(item.nameRu)
     }
 }
