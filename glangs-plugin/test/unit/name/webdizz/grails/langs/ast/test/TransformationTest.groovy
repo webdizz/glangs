a1cc120874e89bde5cfc102fa6072159f1e79166
webdizz <webdizz@gmail.com>
2010-04-23 00:28:31 +0300

    Some formatting

--- glangs-plugin/test/unit/name/webdizz/grails/langs/ast/test/TransformationTest.groovy
+++ glangs-plugin/test/unit/name/webdizz/grails/langs/ast/test/TransformationTest.groovy
@@ -4,17 +4,24 @@ package name.webdizz.grails.langs.ast.test
 import static org.junit.Assert.*
 
 import groovy.lang.MetaClass;
+import name.webdizz.grails.glint.Article;
 import name.webdizz.grails.langs.ast.LocaleTransformation 
 
 import org.codehaus.groovy.ast.builder.TranformTestHelper
 import org.codehaus.groovy.control.CompilePhase 
 //import org.codehaus.groovy.tools.ast.TranformTestHelper
-import org.junit.Test;
+import org.junit.Test
+import groovy.util.ConfigObject
+
+import org.codehaus.groovy.grails.commons.ApplicationHolder;
+import org.codehaus.groovy.grails.commons.cfg.ConfigurationHelper
 
 public class TransformationTest {
 
 	@Test
 	public void testLocalizableTransformation() {
+		def article = new Article()
+		int before = article.metaClass.properties.size()
         def invoker = new TranformTestHelper(new LocaleTransformation(), CompilePhase.CANONICALIZATION)
 
         def file = new File("./grails-app/domain/name/webdizz/grails/glint/Article.groovy")
@@ -27,8 +34,8 @@ public class TransformationTest {
         tester.nameEn = ""
         assertNotNull("There is no property nameEn", tester.nameEn)
         MetaClass mc = tester.metaClass
-        assertTrue("Amount of properties is greater then should be.", mc.properties.size()==8)
-        
+        int after = mc.properties.size()
+        assertTrue("Amount of properties is greater then should be.", after-before==2)
 	}
 
 }
